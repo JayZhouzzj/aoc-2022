319fde4da72189621c6cc78a351b291f45fc8e6e
Zhengjie Zhou (Jay) <zhengjiezhou@dal.ca>
2022-12-16 22:12:07 -0500

    4.2

--- day4p1.cc
+++ day4p1.cc
@@ -7,7 +7,7 @@
 #include <unordered_set>
 
 int overlap(int x1, int y1, int x2, int y2) {
-  return (x1 <= x2 && y1 >= y2) || (x2 <= x1 && y2 >= y1);
+  return (x2 <= y1 && y2 >= x1) || (x1 <= y2 && y1 >= x2);
 }
 
 int main(int argc, char** argv) {
