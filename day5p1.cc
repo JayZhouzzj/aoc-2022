86b9d2b2014f4f3e7634875332101597643c34f1
Jay Zhou <j.zhou@uwaterloo.ca>
2022-12-17 18:18:25 -0500

    5.1

--- day5p1.cc
+++ day5p1.cc
@@ -31,23 +31,22 @@ int main(int argc, char** argv) {
   }
   for (int i = initCount - 1; i >= 0; --i) {
     for (int j = 0; j < stackCount; ++j) {
-      if (init[i].at(j) != ' ') { stacks[j].push(init[i].at(j)); };
+      if (init[i].at(4 * j + 1) != ' ') { stacks[j].push(init[i].at(4 * j + 1)); };
     }
   }
 
   std::getline(f, line);
   std::getline(f, line);
 
+  std::getline(f, line, ' ');
   while (std::getline(f, line, ' ')) {
-    std::cout << line;
-    std::getline(f, line, ' ');
     int count = std::stoi(line);
     std::getline(f, line, ' ');
     std::getline(f, line, ' ');
-    int from = std::stoi(line);
+    int from = std::stoi(line) - 1;
     std::getline(f, line, ' ');
     std::getline(f, line, ' ');
-    int to = std::stoi(line);
+    int to = std::stoi(line.substr(0, 1)) - 1;
 
     for (int i = 0; i < count; ++i) {
       stacks[to].push(stacks[from].top());
